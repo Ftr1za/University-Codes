80702d9075cbdc30a5f5da08192b16d5a6ce0e25
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-03-12 14:57:15 +0600

    Update question on LHW1.java
    Task question is added as a comment.

--- LHW1.java
+++ LHW1.java
@@ -1,3 +1,12 @@
+//NASA has recently shared the total time (in minutes) since one of its space missions was launched.
+//As a software engineer working at the mission control center, your task is to help them calculate how many years and days have passed since the launch.
+//Therefore, write a Java program that can do the conversion.
+//NOTE: For simplicity, assume each year has exactly 365 days.
+//Test Data:
+//Given the number of minutes: 3456789
+//Expected Output:
+//3456789 minutes is approximately 6 years and 210 days
+
 public class LHW1{
   public static void main(String [] args){
     int t_min =  3456789;
@@ -7,3 +16,4 @@ public class LHW1{
     System.out.printf("%d minutes is approximately %d years and %d days.\n", t_min, t_year, r_day);
   }
 }
+
