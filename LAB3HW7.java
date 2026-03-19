d067f4e29aeadc83c5b5160be945134462bc3c91
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-03-20 04:07:38 +0600

    Update and rename L3HW7.java to LAB3HW7.java

--- LAB3HW7.java
+++ LAB3HW7.java
@@ -1,4 +1,6 @@
-public class L3HW7{
+//Write a Java program which adds all numbers that are multiples of either 7 or 9 but not both, up to 600. Use only a single loop in your program.
+
+public class LAB3HW7{
   public static void main(String [] args){
     int sum = 0;
     for (int i =1; i <= 600 ; i++){
