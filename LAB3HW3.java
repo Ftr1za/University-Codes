58b02c2c1be2f6dc7951c5b9de4f1baf2f0bcceb
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-03-20 04:00:03 +0600

    Update and rename L3HW3.java to LAB3HW3.java

--- LAB3HW3.java
+++ LAB3HW3.java
@@ -1,6 +1,11 @@
+// In a sunny mountain cave, a baby dragon named Charmander plays a fun game to evolve. Charmander starts with E energy points.
+// He must blow fire across N small rivers. For each river i, the distance is D units. If D is 5 or less, it costs 2 energy. 
+// If D is more than 5, it costs D / 2 energy (whole numbers only). Take away the energy after each blow. 
+// If energy goes below 0, Charmander gets tired and stops. Tell the river where he stops, or if he finishes all, say how much energy is left.
+
 import java.util.Scanner;
 
-public class L3HW3{
+public class LAB3HW3{
   public static void main(String [] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Energy (E): ");
