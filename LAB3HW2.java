95090791227cfb7bc5abffd1898cfa6a6ea69675
Farhan Tanvir <tanvirfarhan2002@gmail.com>
2026-03-20 03:58:54 +0600

    Update and rename L3HW2.java to LAB3HW2.java

--- LAB3HW2.java
+++ LAB3HW2.java
@@ -5,7 +5,7 @@
 
 import java.util.Scanner;
 
-public class L3HW2{
+public class LAB3HW2{
   public static void main(String [] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Please Enter number of Herbs (H): ");
